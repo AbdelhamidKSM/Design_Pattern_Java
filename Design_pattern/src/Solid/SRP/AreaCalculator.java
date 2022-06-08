d10826a6d52e89174a6769cb3471e622a929a49c
aelkassimi <aelkassimi@sqli.com>
2022-06-08 07:43:22 +0100

    Single Responsability Principale

---
+++ Design_pattern/src/Solid/SRP/AreaCalculator.java
@@ -0,0 +1,15 @@
+package Solid.SRP;
+
+import java.util.List;
+import java.util.Random;
+
+public class AreaCalculator {
+    public int sum() {
+        return new Random().nextInt();
+    }
+    /**
+     Adding methods here do something else except the calculation violates the Single Responsible Principle as :
+     printJsom
+     PrintCsv
+     * */
+}
